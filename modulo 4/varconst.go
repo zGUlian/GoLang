2ff290eeef7328c01a8eff095448210cbfa26930
Gabriel Ulian <gabrielulian55@gmail.com>
2025-04-01 21:46:01 -0300

    Testando e Conhecendo Tipos de Variáveis em GoLang

---
+++ modulo 4/varconst.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// Variáveis
+	// var + nome da variavel + tipo
+
+	var nome string
+	nome = "Gabriel Ulian"
+	fmt.Println(nome)
+
+	nome = "Camile"
+	fmt.Println(nome)
+
+	var idade int
+	idade = 28
+	fmt.Println(idade)
+
+	var a = "Bernardo"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	f := "Apple"
+	fmt.Println(f)
+
+	n := 27
+	fmt.Println(n)
+
+	floa := 2.5
+	fmt.Println(floa)
+
+	const idadeBernardo = 8
+	fmt.Println(idadeBernardo)
+}
