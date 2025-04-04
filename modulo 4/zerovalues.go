19dac5c50cdb89e1392c7853699973ea8943cf9d
Gabriel Ulian <gabrielulian55@gmail.com>
2025-04-03 22:16:25 -0300

    Aprendendo e Manipulando Zero Values

---
+++ modulo 4/zerovalues.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var i int
+	var f float64
+	var b bool
+	var s string
+
+	fmt.Printf("Inteiro: %v\n", i)
+	fmt.Printf("Float: %v\n", f)
+	fmt.Printf("Booleana: %v\n", b)
+	fmt.Printf("String: %v\n", s)
+
+}
